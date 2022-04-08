2a4a53f2ef50780844ef316d6ced76f8e9dc4817
Marina <marina.alapont@estudiantat.upc.edu>
2022-04-08 11:18:25 +0000

    Task#22_Modifica l'estetica del form

--- db/schema.rb
+++ db/schema.rb
@@ -21,11 +21,4 @@ ActiveRecord::Schema.define(version: 2022_04_06_180615) do
     t.string "text", default: ""
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
 end
