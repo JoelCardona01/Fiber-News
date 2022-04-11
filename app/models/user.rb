f37f8b4d4fdcab10703a450065453145473c5294
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-04-11 19:29:37 +0000

    Fa que la funcionalitat de veure usuari funcioni correctament

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
     
-    def self.update(params)
-      user.about = params.about
-    end
     
     def self.from_omniauth(auth)
       where(email: auth.info.email).first_or_initialize do |user|
