7aca1002a997d764b357a70741be643d2c3bd3f7
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-04-11 17:49:50 +0000

    Modifica la vista d'editar perfil

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ApplicationRecord
     
-    def self.from_omniauth(auth)
-    where(email: auth.info.email).first_or_initialize do |user|
-      user.name = auth.info.first_name
-      user.email = auth.info.email
+    def self.update(params)
+      user.about = params.about
     end
-  end
+    
+    def self.from_omniauth(auth)
+      where(email: auth.info.email).first_or_initialize do |user|
+        user.name = auth.info.name
+        user.email = auth.info.email
+      end
+    end
   
 end
