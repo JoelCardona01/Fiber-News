85120190d96610d8cb5bf939715a2d720eccca5a
JoelCS <joel.cardona@estudiantat.upc.edu>
2022-04-12 15:34:26 +0000

    Task#33_Crea reply i fa la pantalla de mostra d'un comentari

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
     get '/submissions/ask', to: 'submissions#ask', as: 'ask_submissions'
     post '/submissions/comment', to: 'submissions#comment', as: 'submissions_comment'
     get '/sessions/logout', to: 'sessions#logout', as: 'logout_sessions'
-    
+    post 'comments/comment', to: 'comments#comment', as: 'comments_comment'
     #Google login
     get '/auth/:provider/callback' => 'sessions#omniauth'
 
