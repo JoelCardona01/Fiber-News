f6490553f2111947da0940085e4c6bf1be61247d
daniel.pulido.galvez <daniel.pulido.galvez@estudiantat.upc.edu>
2022-04-13 17:01:50 +0000

    Fa que les submissions tinguin usuari creador i arregla les vistes d'usuari

--- db/migrate/20220411090923_create_comments.rb
+++ db/migrate/20220411090923_create_comments.rb
@@ -1,6 +1,7 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
+      t.belongs_to :user
       t.string :text
       t.integer :postid
       t.integer :parentid
