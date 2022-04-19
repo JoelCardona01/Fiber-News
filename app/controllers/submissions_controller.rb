ea682a84ace1bc31aa7e54b4265ee8015e7e4df7
Marina <marina.alapont@estudiantat.upc.edu>
2022-04-19 22:52:47 +0000

    Task#46_Permet editar comentaris i arregla alguns errors de display

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -110,7 +110,7 @@ class SubmissionsController < ApplicationController
   def update
     respond_to do |format|
       if @submission.update(submission_params)
-        format.html { redirect_to @submission, notice: 'Submission was successfully updated.' }
+        format.html { redirect_to @submission }
         format.json { render :show, status: :ok, location: @submission }
       else
         format.html { render :edit }
