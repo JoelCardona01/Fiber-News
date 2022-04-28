774c3441ef64d602f88467f3c2ad2ab224f07a15
JoelCS <joel.cardona@estudiantat.upc.edu>
2022-04-28 09:43:48 +0000

    Task#87_Afegir el retorn del format json de les submissions d'un usuari donat

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -36,6 +36,10 @@ class SubmissionsController < ApplicationController
     if !session[:user_id].nil?
       @likedsubmissions = Likedsubmission.all.where(:user_id => session[:user_id])
     end
+    respond_to do |format|
+            format.html {  }
+            format.json { render json: @submissions }
+    end
     render "index"
   end
   
