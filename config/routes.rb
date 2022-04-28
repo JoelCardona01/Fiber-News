dcd7d98334b76ff4e9d7478ba13c59591c04a41b
JoelCS <joel.cardona@estudiantat.upc.edu>
2022-04-28 09:53:59 +0000

    Modificar la ruta i crear una nova funcio al controller

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     
     #Rutas de la api
     get '/api/submissions', to: 'submissions#index', as: 'index'
-    get '/api/submissions/user/:user_id', to: 'submissions#submFromUrl', as: 'subm_user'
+    get '/api/submissions/user/:user_id', to: 'submissions#submFromUrlJSON', as: 'subm_user_JSON'
     
     #Google login
     get '/auth/:provider/callback' => 'sessions#omniauth'
