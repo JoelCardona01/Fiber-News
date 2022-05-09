ca3d0f469df20911ccde29801fae55e9f4873544
SimonOliva <simon.helmuth.oliva@estudiantat.upc.edu>
2022-05-09 21:08:03 +0000

    ja es pot fer get d'una submission i dels comentaris d'una submission (falta el control d'errors)

--- app/controllers/submissions_controller.rb
+++ app/controllers/submissions_controller.rb
@@ -1,6 +1,6 @@
 class SubmissionsController < ApplicationController
   before_action :set_submission, only: [:show, :edit, :update, :destroy, :vote, :unvote]
-
+  skip_before_action :verify_authenticity_token
   # GET /submissions
   # GET /submissions.json
   def index
@@ -8,14 +8,16 @@ class SubmissionsController < ApplicationController
     if !session[:user_id].nil?
       @likedsubmissions = Likedsubmission.all.where(:user_id => session[:user_id])
     end
+     respond_to do |format|
+            format.html {  }
+            format.json { render json: @submissions }
+    end
   end
   
-  # GET /api/submissions
-    def indexAPI
+   # GET /api/submissions
+  def indexAPI
     @submissions = Submission.all.order(votes: :desc)
-    if !session[:user_id].nil?
-      @likedsubmissions = Likedsubmission.all.where(:user_id => session[:user_id])
-    end
+  
      respond_to do |format|
         format.json { render json: @submissions }
     end
@@ -30,16 +32,26 @@ class SubmissionsController < ApplicationController
       @likedcomments = Likedcomments.all.where(:user_id => session[:user_id])
     end
   end
-  
-  # GET /submissions/1
-  # GET /submissions/1.json
+
+
+  # GET /api/submissions/:post_id
   def submission_JSON
+    @submissions = Submission.all.where(:id => params[:post_id])
+  
+    respond_to do |format|
+      format.json { render json: @submissions }
+    end
+  end
+  
+  # GET /api/submissions/:post_id/comments
+  def sub_comments_JSON
     @comments= Comment.all.where(:postid => params[:post_id]).order(:parentid)
+  
     respond_to do |format|
       format.json { render json: @comments }
     end
   end
-
+  
   
   # GET /submissions/new
   def new
@@ -55,10 +67,11 @@ class SubmissionsController < ApplicationController
     render "index"
   end
   
+  #GET /api/submissions/user/:user_id
   def submFromUserJSON
     respond_to do |format|
       if User.find_by(:id => params[:user_id]).nil?
-        format.json { render json: {"status": 410, "error": "User does not exist", "message": "There is no user with same user_id as provided"}, status: 410
+        format.json { render json: {"status": 433, "error": "User does not exists", "message": "There is no user with same user_id as provided"}, status: 433
           return } 
       else 
         @submissions = Submission.all.where(:user_id => params[:user_id]).order(created_at: :desc)
@@ -83,6 +96,26 @@ class SubmissionsController < ApplicationController
     @submissions = Likedsubmission.all.where(:user_id => session[:user_id]).order(created_at: :desc)
     render :upvotes
   end
+  
+  #GET /api/submissions/upvoted/user/:user_id
+  def userUpvotesJSON
+    respond_to do |format|
+      if User.find_by(:id => params[:user_id]).nil?
+        format.json { render json: {"status": 433, "error": "User does not exists", "message": "There is no user with same user_id as provided"}, status: 433
+          return } 
+      else 
+        @likedsubmissions = Likedsubmission.all.where(:user_id => params[:user_id]).order(created_at: :desc)
+        format.json { 
+          submissions = []
+          @likedsubmissions.each do |ls|
+            submissions.push(Submission.find_by(:id => ls.submission_id))
+          end
+          render json: submissions.to_json, status: :ok
+        }
+      end
+    end
+  end
+  
 
   # GET /submissions/1/edit
   def edit
@@ -113,73 +146,12 @@ class SubmissionsController < ApplicationController
       elsif (@submission.url=="") 
         if @submission.save
             format.html { redirect_to submissions_path }
-<<<<<<<<< saved version
-  
-  def newest
-        @submissions = Submission.all.order(created_at: :desc)
-        if !session[:user_id].nil?
-          @likedsubmissions = Likedsubmission.all.where(:user_id => session[:user_id])
-        end
-
-  end
-
-  def ask
-      @submissions = Submission.all.where(:url=>"").order(votes: :desc)
-      if !session[:user_id].nil?
-        @likedsubmissions = Likedsubmission.all.where(:user_id => session[:user_id])
-      end
-  end
-  
-  # POST /submissions
-  # POST /submissions.json
-  def create
-    @submission = Submission.new(submission_params)
-    
-    respond_to do |format|
-      
-      if @submission.title=="" then format.html { redirect_to request.referrer, alert: 'That is not a valid title.' } ##Comprovem que el titol no es buit
-      elsif (@submission.url=="") 
-        if @submission.save
-            format.html { redirect_to submissions_path }
             format.json { render :show, status: :created, location: @submission }
-=========
-            format.json { render @submission, status: :created, location: @submission }
->>>>>>>>> local version
           else
             format.html { render :new }
             format.json { render json: @submission.errors, status: :unprocessable_entity }
           end
       elsif ((@submission.url!="" && Submission.find_by(url: @submission.url).nil?))##Comprovem que no existeixi cap submission amb el mateix url i guardem la nova
-        if @submission.text!=""
-          text = @submission.text
-          @submission.text=""
-          if @submission.save
-            @comment= Comment.new(:text => text, :user_id =>@submission.user_id, :postid => @submission.id, :parentid => "0", :likes => 0)
-            @comment.save
-            format.html { redirect_to submissions_path }
-            format.json { render :show, status: :created, location: @submission }
-          else
-            format.html { render :new }
-            format.json { render json: @submission.errors, status: :unprocessable_entity }
-          end
-        else
-          if @submission.save
-            format.html { redirect_to submissions_path }
-            format.json { render :show, status: :created, location: @submission }
-          else
-            format.html { render :new }
-            format.json { render json: @submission.errors, status: :unprocessable_entity }
-          end
-        end
-       
-      else ##Ja existia una submission amb el mateix url de manera que redirigim a la submission amb el mateix url.
-        @submission2 = Submission.find_by(url: @submission.url)
-        format.html {redirect_to @submission2 }
-      end
-       
-    end 
-  end
-
         if @submission.text!=""
           text = @submission.text
           @submission.text=""
@@ -305,239 +277,6 @@ class SubmissionsController < ApplicationController
             "status":403,
             "error": "Forbidden",
             "message": "You cannot vote your own submission"
-          },
-          status: 403
-        }
-      end
-      return
-    end
-    @user = User.all.where(:APIKey => request.headers["X-API-Key"]).first()
-    if not Likedsubmission.all.find_by(user_id: @user.id, submission_id: params[:submission_id]).nil?
-      respond_to do |format|
-        format.json{
-          render json: {
-            "status":403,
-            "error": "Forbidden",
-            "message": "You cannot vote twice a submission"
-          },
-          status: 403
-        }
-      end
-      return
-    end
-    @submission = Submission.find_by(id: params[:submission_id])
-    @submission.votes = @submission.votes+1
-    if @submission.save
-      @likedsubmission = Likedsubmission.new(:submission_id => @submission.id, :user_id => @user.id)
-      if @likedsubmission.save
-        respond_to do |format|
-        format.json { 
-          render json: {
-            "status":200,
-            "comment":@submission,
-            "message": "Submission voted",
-          },
-          status: 200
-        }
-        end
-      else 
-        respond_to do |format|
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
-        end
-      end
-    else
-      respond_to do |format|
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
-  #DELETE /api/submissions/:submission_id/vote
-  def APIUnvote
-        if request.headers["X-API-KEY"].nil? or request.headers["X-API-KEY"].blank? then
-      respond_to do |format|
-        format.json{
-         render json: {
-          "status":401,
-          "error": "Unauthorized",
-          "message": "You provided no api key (X-API-KEY Header)"
-        },
-        status: 401
-        }
-      end
-      return
-    end
-    if User.find_by(:APIKey => request.headers["X-API-Key"]).nil?
-      respond_to do |format|
-        format.json{
-          render json: {
-            "status":403,
-            "error": "Forbidden",
-            "message": "Your api key (X-API-KEY Header) is not valid"
-          },
-          status: 403
-        }
-      end
-      return
-    end
-    if (Submission.find_by(id: params[:submission_id]).nil?)
-      respond_to do |format|
-        format.json{
-        render json: {
-          "status":404,
-          "error": "Not Found",
-          "message": "Submission not found"
-        },
-        status: 404
-        }
-        end
-    return
-    end
-    if request.headers["X-API-Key"] == Submission.find_by(id: params[:submission_id]).user.APIKey
-      respond_to do |format|
-        format.json{
-          render json: {
-            "status":403,
-            "error": "Forbidden",
-            "message": "You cannot unvote your own submission and will never be ablo to because you can't vote your own submission"
-          },
-          status: 403
-        }
-      end
-      return
-    end
-    @user = User.all.where(:APIKey => request.headers["X-API-Key"]).first()
-    if Likedsubmission.all.find_by(user_id: @user.id, submission_id: params[:submission_id]).nil?
-      respond_to do |format|
-        format.json{
-          render json: {
-            "status":403,
-            "error": "Forbidden",
-            "message": "You cannot unvote a submission that you haven't voted"
-          },
-          status: 403
-        }
-      end
-      return
-    end
-    @submission = Submission.find_by(id: params[:submission_id])
-    @submission.votes = @submission.votes-1
-    if @submission.save
-      @likedsubmission = Likedsubmission.find_by(:submission_id => @submission.id, :user_id => @user.id)
-      @likedsubmission.destroy
-      respond_to do |format|
-        format.json { 
-          render json: {
-            "status":200,
-            "comment":@submission,
-            "message": "Submission unvoted",
-          },
-          status: 200
-        }
-      end
-    else
-      respond_to do |format|
-      format.json { render json: @comment.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-  
-  
-  def unvote
-    @submission.votes = @submission.votes - 1
-    if @submission.save
-      @likedsubmission = Likedsubmission.find_by(:submission_id => @submission.id, :user_id => session[:user_id])
-      @likedsubmission.destroy
-    end
-    redirect_to request.referrer
-  end
-  
-  def 
-  
-  #POST /api/submissions/:submission_id/comment
-  def commentAPI
-    respond_to do |format|
-      if request.headers["X-API-KEY"].nil? or request.headers["X-API-KEY"].blank? then
-        format.json{
-           render json: {
-            "status":401,
-            "error": "Unauthorized",
-            "message": "You provided no api key (X-API-KEY Header)"
-          },
-          status: 401
-          return
-      }
-      elsif User.find_by(:APIKey => request.headers["X-API-Key"]).nil?
-          format.json{
-            render json: {
-              "status":403,
-              "error": "Forbidden",
-              "message": "Your api key (X-API-KEY Header) is not valid"
-            },
-            status: 403
-          }
-      else
-        @user = User.all.where(:APIKey => request.headers["X-API-Key"]).first()
-        params.permit!
-        if !params[:text].blank? ##Si el text no es buit, aleshores creem el comentari.
-            @comment = Comment.new
-            @comment.user_id = @user.id
-            @comment.text = params[:text]
-            @comment.postid = params[:submission_id]
-            @comment.parentid = 0
-            if Submission.find_by(:id => params[:submission_id]).nil? then
-              format.json{
-                 render json: {
-                  "status":432,
-                  "error": "Submission not found",
-                  "message": "It does not exists any submission with the same id as you provided in the query parameters"
-                },
-                status: 432
-              }
-            else
-              if @comment.save
-                format.json { 
-                  render json: {
-                    "status":201,
-                    "comment":@comment,
-                    "message": "Comment posted",
-                  },
-                  status: :ok
-                }
-              else
-              format.json { render json: @comment.errors, status: :unprocessable_entity }
-              end
-            end
-        end
-      end
-    end
-  end
-  
-  def comment
-    params.permit!
-    if !params[:comment][:text].blank? ##Si el text no es buit, aleshores creem el comentari.
-      @comment = Comment.new(params[:comment])
-      respond_to do |format|
-        if @comment.save
-          format.html { redirect_to "/submissions/"+@comment.postid.to_s }
-          format.json { render :show, status: :created, location: @comment }
-        else
-          format.html { render :new }
-          format.json { render json: @comment.errors, status: :unprocessable_entity }
-        end
-      end
-    end
-  end
-  
-
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_submission
-      @submission = Submission.find(params[:id])
-    end
-
-    # Only allow a list of trusted parameters through.
-    def submission_params
           },
           status: 403
         }
