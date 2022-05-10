861a77c0808367bc2367bd922b6a16a514ce9c95
Marina <marina.alapont@estudiantat.upc.edu>
2022-05-10 12:59:19 +0000

    Task#86_Afegeix un default a url

--- db/migrate/20220406152826_create_submissions.rb
+++ db/migrate/20220406152826_create_submissions.rb
@@ -2,7 +2,7 @@ class CreateSubmissions < ActiveRecord::Migration[6.1]
   def change
     create_table :submissions do |t|
       t.belongs_to :user
-      t.string :url
+      t.string :url, default: ""
       t.string :title
       t.timestamps
     end
